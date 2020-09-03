9578ae4a2a621164e41c96d02a36c0c9427c12f5
huzaifa <huzaifa98nur@gmail.com>
2020-09-03 15:56:49 +0500

    Die Roll app

---
+++ die-roll/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RollDie from "./RollDice";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <RollDie face="five" />
+    </div>
+  );
+}
+
+export default App;
